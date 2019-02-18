dffcf3284e5eb2b3724bc60de2dc59b3e364fcbd
andreapasquali <squalipa@yahoo.it>
2019-02-18 12:13:31 +0100

    oneHotEncoding prove varie

--- oneHotEncoding.py
+++ oneHotEncoding.py
@@ -4,22 +4,23 @@ from tabulate import tabulate
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 import numpy as np
-from pandas import pandas
+from sklearn import preprocessing
+import pandas as pd
 from sklearn import tree
 from pydotplus import pydotplus
 import collections
-import graphviz
 
 connection = DBConnection()
 connection.database_connection()
 
 connection = DBConnection()
 connection.database_connection()
-query = "SELECT  age, race, id FROM censusdata limit 15"
+query = "SELECT  age, race FROM censusdata limit 5"
 result = connection.query(query)
-print(tabulate(result,  tablefmt="fancy_grid"))      #https://pypi.org/project/tabulate/ per info su tabulate
+print(tabulate(result,  tablefmt="fancy_grid"))
 
-the_frame = pandas.DataFrame.from_dict(result)
+
+the_frame = pd.DataFrame(result)
 
 print(the_frame)
 print(the_frame.dtypes)
@@ -28,15 +29,39 @@ print(the_frame.dtypes)
 #print(the_frame.index)
 #print(the_frame.sort_values(by='age'))      #ordina table per value
 
+X = the_frame[["race"]]
+y = [0,1,0,1,0]         #sopra i 50 anni
+
+race = the_frame.race.unique()
+print(the_frame.race)
+
+enc = preprocessing.OneHotEncoder(categories=[race])
+encoded = enc.fit_transform(X).toarray()
+print(encoded)
+#print(enc.inverse_transform(encoded))          #ritrasforma con valori iniziali
+
+print(the_frame)
+the_frame = the_frame.drop(columns=['race'])
+print(the_frame)
+
+
+encoded_data_frame = pd.DataFrame(encoded)
+
+print(encoded_data_frame)
+
+the_frame = pd.concat([the_frame, encoded_data_frame], axis=1)
+
+print(the_frame)
+
+print(the_frame[0])
 
 classifier = tree.DecisionTreeClassifier()
-classifier.fit(the_frame[["age"]], the_frame["id"])
+classifier.fit(the_frame.drop(columns=['age']), y)
 print(classifier)
 
 
 # Visualize data
 dot_data = tree.export_graphviz(classifier,
-                                feature_names=["age"],
                                 out_file=None,
                                 filled=True,
                                 rounded=True)
