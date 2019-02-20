a44eeb6b468b4e4fbeeccc7937a91a21341d4ff9
andreapasquali <squalipa@yahoo.it>
2019-02-20 15:58:53 +0100

    varie

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ connection = DBConnection()
 connection.database_connection()
 print("scrivi la query per ricavare le tuple che vuoi analizzare (la X).  es: SELECT * FROM censusdata limit 4")
 #query = input()
-query = 'SELECT age, occupation, race FROM censusdata limit 10'
-table = connection.query(query)
+query_x = 'SELECT age, occupation, race FROM censusdata limit 20'
+table_x = connection.query(query_x)
+table_y = connection.query('SELECT age, occupation, race FROM censusdata where id=3 or id=14')
 print('ecco le tuple: \n ' + tabulate(table, tablefmt="fancy_grid"))
 
 """" Per estrarre una colonna dei risultati
 column_id = [item['id'] for item in result]
 column_Age = [item['Age'] for item in result"""
 
-y=[0,1,1,1,0,1,0,0,1,0]
+y=[0,1,1,1,0,1,0,0,1,0,0,1,1,1,0,1,0,0,1,0]
 
-x = pd.DataFrame(table)
+x = pd.DataFrame(table_x)
 encod = OneHotEncoding()
 encoded = encod.encoder(x)
 print(encoded)
