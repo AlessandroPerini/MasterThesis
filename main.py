762e8236abe35c0db1bc37d0cbce3118e32cbb63
andreapasquali <squalipa@yahoo.it>
2019-02-25 15:08:44 +0100

    free tuple selction random

--- main.py
+++ main.py
@@ -17,22 +17,24 @@ column_id = [item['id'] for item in result]
 column_Age = [item['Age'] for item in result
 """
 
-df_x = pd.DataFrame(connection.query("select * from censusdata where id < 100"))
-df_y = pd.DataFrame(connection.query("select race,age,sex from censusdata where id = 3 or id = 4"))
+df_x = pd.DataFrame(connection.query("select age, sex from censusdata where id < 100"))
+df_y = pd.DataFrame(connection.query("select age, sex from censusdata where id = 3 or  id = 4"))
 y = Utility().transform_y_to_all_results(df_x, df_y)
-y = y.drop(axis=1, columns=["tupleset","isfree"])
+print(y)
+
+y = Utility().free_tuple_selection_random(y)
+print(y)
+
 
 vect_y = Utility().y_creator(df_x, y)
-
 enc = OneHotEncoding()
 encoded = enc.encoder(df_x)
 print(encoded)
-
 classifier = tree.DecisionTreeClassifier()
 classifier.fit(encoded, vect_y)
 print(classifier)
-
 headers = list(encoded.columns.values)
 Utility().tree_printer(classifier, headers)
+
 
 connection.close_connection()
