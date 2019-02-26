00f032876f28309c23c40db8330bcad6afbff583
andreapasquali <squalipa@yahoo.it>
2019-02-26 10:11:06 +0100

    main riordinato

--- main.py
+++ main.py
@@ -8,34 +8,22 @@ import sklearn
 
 connection = DBConnection()
 connection.database_connection()
-print("scrivi la query per ricavare le tuple che vuoi analizzare (la X).  es: SELECT * FROM censusdata limit 4")
-query = 'SELECT age, occupation, race FROM censusdata limit 10'
-table = connection.query(query)
-print('ecco le tuple: \n ' + tabulate(table, tablefmt="fancy_grid"))
-
-"""" Per estrarre una colonna dei risultati
-column_id = [item['id'] for item in result]
-column_Age = [item['Age'] for item in result
-"""
-
 df_x = pd.DataFrame(connection.query("select age, capitalgain, capitalloss, hoursperweek from censusdata where id < 100"))
 df_y = pd.DataFrame(connection.query("select age, hoursperweek from censusdata where id = 3 or  id = 4 or  id = 7  or  id = 22  or  id = 55"))
-y = Utility().transform_y_to_all_results(df_x, df_y)
-print(y)
-
-y = Utility().free_tuple_selection_random(y)
-print(y)
-
-
+print('this is yor result table:')
+print(df_y)
+y = Utility().transform_y_to_all_results(df_x, df_y)    #transforms y so that it will have all the attributes as X
+print('do you prefer a random or a cluster choice of the free tuples? (r / c)')
+random_cluster = input()
+if random_cluster == 'r':
+    y = Utility().free_tuple_selection_random(y)
+else:
+    y = Utility().free_tuple_selection_cluster(y)
 vect_y = Utility().y_creator(df_x, y)
-enc = OneHotEncoding()
-encoded = enc.encoder(df_x)
-print(encoded)
+encoded = OneHotEncoding().encoder(df_x)
 classifier = tree.DecisionTreeClassifier()
 classifier.fit(encoded, vect_y)
-print(classifier)
 headers = list(encoded.columns.values)
 Utility().tree_printer(classifier, headers)
-
-
+print('tree printed in png')
 connection.close_connection()
