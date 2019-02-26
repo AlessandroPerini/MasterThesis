954544066dd5ce09fb0eb4c9a4e7598e5971c027
Alessandro Perini <alessandroperini@hotmail.it>
2019-02-26 10:07:20 +0100

    Pulizia codice e commenti alle funzioni in utility

--- main.py
+++ main.py
@@ -18,14 +18,15 @@ column_id = [item['id'] for item in result]
 column_Age = [item['Age'] for item in result
 """
 
-df_x = pd.DataFrame(connection.query("select age, capitalgain, capitalloss, hoursperweek from censusdata where id < 100"))
-df_y = pd.DataFrame(connection.query("select age, hoursperweek from censusdata where id = 3 or  id = 4 or  id = 7  or  id = 22  or  id = 55"))
+df_x = pd.DataFrame(
+    connection.query("select age, capitalgain, capitalloss, hoursperweek from censusdata where id < 100"))
+df_y = pd.DataFrame(connection.query(
+    "select age, hoursperweek from censusdata where id = 3 or  id = 4 or  id = 7  or  id = 22  or  id = 55"))
 y = Utility().transform_y_to_all_results(df_x, df_y)
 print(y)
 
-y = Utility().free_tuple_selection_random(y)
+y = Utility().free_tuple_selection_cluster(y)
 print(y)
-
 
 vect_y = Utility().y_creator(df_x, y)
 enc = OneHotEncoding()
@@ -36,6 +37,5 @@ classifier.fit(encoded, vect_y)
 print(classifier)
 headers = list(encoded.columns.values)
 Utility().tree_printer(classifier, headers)
-
 
 connection.close_connection()
--- utility.py
+++ utility.py
@@ -4,14 +4,13 @@ from sklearn import tree
 import pandas as pd
 from random import randint
 from sklearn.cluster import KMeans
-import numpy as np
+
 
 class Utility:
 
     @staticmethod
     def tree_printer(classifier, features):
         """
-
         :param classifier: variable in witch is built the decision tree
         :param features: headers of columns of dataframe x
         :return: nothing, it prints the png image with the tree
@@ -49,7 +48,7 @@ class Utility:
         :param dataframe_y: dataframe with tuples that are congruent with the result
         :return: y vector
 
-        Note: dataframe_x and dataframe_y must have the same number of parameters (column)
+        Note: dataframe_x must have less column than dataframe_y
         """
 
         dataframe_x = dataframe_x.sort_values(by=dataframe_x.columns.tolist())
@@ -85,12 +84,11 @@ class Utility:
         """
         result = pd.DataFrame()
         for row in range(dataframe_results.shape[0]):
-
             new_rows = dataframe_x
 
             for column in range(len(dataframe_results.columns)):
-
-                new_rows = new_rows[(new_rows[dataframe_results.columns.values[column]] == dataframe_results.iloc[row, column])]
+                new_rows = new_rows[
+                    (new_rows[dataframe_results.columns.values[column]] == dataframe_results.iloc[row, column])]
 
             new_rows['tupleset'] = [row] * new_rows.shape[0]
 
@@ -106,41 +104,42 @@ class Utility:
     @staticmethod
     def free_tuple_selection_random(dataframe_y):
         """
-
-        :param dataframe_y: is the dataframe from wich we want to select randomly the tuple of every set of free tuples
-        :return: dataframe y only with positive tuples and without columns 'isfree' and 'tupleset'
+        :param dataframe_y: dataframe from which we want to select randomly the tuple of every set of free tuples
+        :return: dataframe y only with positive tuples
         """
         result = pd.DataFrame()
         for set_index in range(len(dataframe_y.tupleset.unique())):
             tmp = dataframe_y[(dataframe_y.tupleset == set_index)]
+
             if tmp.shape[0] != 1:
-                tmp = tmp.iloc[[randint(0, tmp.shape[0]-1)]]
+                tmp = tmp.iloc[[randint(0, tmp.shape[0] - 1)]]
+
             result = pd.concat([result, tmp], axis=0)
 
-        print(result)       #printa la tabella di y con ancora le colonne 'tupleset' e 'isfree'
-        result = result.drop(axis=1, columns=["tupleset", "isfree"])
+        # result = result.drop(axis=1, columns=["tupleset", "isfree"])
+        # Per ora non serve...
         return result
 
     @staticmethod
     def free_tuple_selection_cluster(dataframe_y):
+        """
+        :param dataframe_y: dataframe from which we want to select the tuple of every set of free tuples using KMeans clusters
+        :return: dataframe y only with positive tuples
+        """
         n_clusters = 3
         kmeans = KMeans(n_clusters=n_clusters).fit(dataframe_y)
-        unique, counts = np.unique(kmeans.labels_, return_counts=True)
         print(kmeans.labels_)
         dataframe_y['cluster'] = kmeans.labels_
-        print(dataframe_y)
         result = pd.DataFrame()
+
         while dataframe_y.shape[0]:
-            tmp_cluster = dataframe_y.cluster.mode().values[0]  #takes the biggest cluster with more tuples
+            tmp_cluster = dataframe_y.cluster.mode().values[0]  # takes the biggest cluster
             tmp = dataframe_y[(dataframe_y.cluster == tmp_cluster)]
             dataframe_y = dataframe_y[dataframe_y.cluster != tmp_cluster]
-            print(dataframe_y)
+
             for set in tmp.tupleset.unique():
                 rows = tmp[tmp.tupleset == set]
                 result = pd.concat([result, rows.iloc[[0]]], axis=0)
                 dataframe_y = dataframe_y[dataframe_y.tupleset != set]
-            print(tmp)
-            print(dataframe_y)
-        print("RESULT")
-        print(result)
+
         return result
