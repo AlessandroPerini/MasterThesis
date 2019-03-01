8ad82689217229932b4776c800dfe82b6d70f688
Alessandro Perini <alessandroperini@hotmail.it>
2019-03-01 13:21:55 +0100

    Creazione path_compresser

--- utility.py
+++ utility.py
@@ -12,14 +12,12 @@ class Utility:
 
     tree_path = 'default.png'
 
-    def tree_printer(self, classifier, dataframe_x, list_y, tree_path='default.png'):
+    def tree_printer(self, classifier, dataframe_x, tree_path='default.png'):
         """
         :param classifier: variable in witch is built the decision tree
         :param features: headers of columns of dataframe x
         :return: nothing, it prints the png image with the tree
         """
-        most_important_nodes = self.most_important_nodes_generator(list_y, classifier, dataframe_x)
-
         if tree_path != 'default.png':
             self.tree_path = tree_path
         try:
@@ -39,40 +37,16 @@ class Utility:
                 edges[edge.get_source()].append(int(edge.get_destination()))
 
             for edge in edges:
-                #print(edges)
-                #print(type(edges))
                 edges[edge].sort()
                 for i in range(2):
                     dest = graph.get_node(str(edges[edge][i]))[0]
                     dest.set_fillcolor(colors[i])
-                    """
-                    print('dir (dest)')
-                    print(dir(dest))
-                    print('class:')
-                    print(dest.get_URL)
-                    print('get_pos:')
-                    print(dest.get_pos)
-                    print('get_name')
-                    print(str(dest.get_name))
-                    print('get_id')
-                    print(str(dest.get_id))
-                    print('get_fillcolor')
-                    print(dest.get_fillcolor)
-                    """
+
             graph.write_png(self.tree_path)
             print("Tree " + self.tree_path + " printed!")
 
         except ValueError as ve:
             print("Tree not printed!" + ve)
-
-    @staticmethod
-    def most_important_nodes_generator(list_y, classifier, dataframe_x):
-        applied = classifier.apply(dataframe_x)
-        important_nodes = list()
-        for elem in range(len(list_y)):
-            if list_y[elem] != 0:
-                important_nodes.append(applied[elem])
-        return important_nodes
 
     @staticmethod
     def y_creator(dataframe_x, dataframe_y):
@@ -87,31 +61,23 @@ class Utility:
         dataframe_x = dataframe_x.sort_values(by=dataframe_x.columns.tolist())
         dataframe_y = dataframe_y.sort_values(by=dataframe_x.columns.tolist())
 
-        print('check questi due:')
-        print(dataframe_x)
-        print(dataframe_y)
-
         col = len(dataframe_x.columns)
         y = [1] * dataframe_x.shape[0]
         count = 0
-        print("shape di y=")
-        print(dataframe_y.shape[0])
+        row_index = 0
         for row in range(dataframe_x.shape[0]):
             if count < dataframe_y.shape[0]:
+                row_index = row
                 for c in range(col):
                     if dataframe_x.iloc[row, c] != dataframe_y.iloc[count, c]:
                         y[row] = 0
 
                 if y[row] == 1:
                     count += 1
-            else:
-                for r in range(row, dataframe_x.shape[0]):
-                    y[r] = 0
-            print(count)
-            print(row)
-        print('risultati dell y creator')
-        print(y)
-        print(dataframe_y)
+
+        for r in range(row_index+1, dataframe_x.shape[0]):
+            y[r] = 0
+
         return dataframe_x, dataframe_y, y
 
     @staticmethod
@@ -125,6 +91,7 @@ class Utility:
         result = pd.DataFrame()
         for row in range(dataframe_results.shape[0]):
             new_rows = dataframe_x
+
             for column in range(len(dataframe_results.columns)):
                 new_rows = new_rows[
                     (new_rows[dataframe_results.columns.values[column]] == dataframe_results.iloc[row, column])]
@@ -190,8 +157,7 @@ class Utility:
         print(result)
         return result
 
-    @staticmethod
-    def path_finder(classifier, dataframe_x, list_y):
+    def path_finder(self, classifier, dataframe_x, list_y):
         """
         :param classifier: classifier
         :param dataframe_x: dataframe x
@@ -205,6 +171,7 @@ class Utility:
         features = classifier.tree_.feature
         thresholds = classifier.tree_.threshold
 
+        dictionaries_explanation = set()
         explanations = list()
         for elem in range(len(list_y)):
             if list_y[elem] != 0:
@@ -214,6 +181,7 @@ class Utility:
                 node_path = list()
                 attribute_path = list()
                 threshold_path = list()
+                dictionaries = list()
 
                 for index in range(len(tmp_matrix)):
                     if tmp_matrix[index] != 0:
@@ -222,12 +190,18 @@ class Utility:
                         if features[index] != -2:
                             attribute_path.append(headers[features[index]])
                             threshold_path.append(thresholds[index])
+                            dictionary = {'column': headers[features[index]],
+                                          'symbol': '',
+                                          'value': thresholds[index]}
+                            dictionaries.append(dictionary)
 
                 explanation = list()
                 for index in range(len(node_path) - 1):
                     if children_left[node_path[index]] == node_path[index + 1]:
+                        dictionaries[index]['symbol'] = '<='
                         explanation.append(attribute_path[index] + ' <= ' + str(threshold_path[index]))
                     else:
+                        dictionaries[index]['symbol'] = '>'
                         explanation.append(attribute_path[index] + ' > ' + str(threshold_path[index]))
 
                 explanation_string = ''
@@ -237,9 +211,66 @@ class Utility:
                     explanation_string += explanation[index]
 
                 explanations.append(explanation_string)
+                compressed_dictionaries = self.path_compresser(dictionaries)
+                string_compressed_dictionaries = self.from_dictionaries_to_string(compressed_dictionaries)
+                dictionaries_explanation.add(string_compressed_dictionaries)
 
-        return explanations
+        return explanations, dictionaries_explanation
 
+    def path_compresser(self, dictionaries):
+        compressed_dictionaries = list()
+        columns_set = set()
+        print("Dizionari di partenza:")
+        for dictionary in dictionaries:
+            columns_set.add(dictionary['column'])
+
+            print(dictionary)
+
+        for unique_column in columns_set:
+            unique_dictionary = list()
+            for dictionary in dictionaries:
+                if dictionary['column'] == unique_column:
+                    unique_dictionary.append(dictionary)
+
+            greater_dict = list()
+            less_dict = list()
+            for dictionary in unique_dictionary:
+                if dictionary['symbol'] == '>':
+                    greater_dict.append(dictionary['value'])
+                else:
+                    less_dict.append(dictionary['value'])
+
+            if len(greater_dict) > 0:
+                greater_dict.sort(reverse=True, key=float)
+                dictionary = {'column': unique_column,
+                             'symbol': '>',
+                             'value': greater_dict[0]}
+                compressed_dictionaries.append(dictionary)
+
+            if len(less_dict) > 0:
+                less_dict.sort(key=float)
+                dictionary = {'column': unique_column,
+                             'symbol': '<=',
+                             'value': less_dict[0]}
+                compressed_dictionaries.append(dictionary)
+
+        print("\n" + 'Dizionari compressi:')
+        for d in compressed_dictionaries:
+            print(d)
+
+        print("\n" + "---------------------------------" + "\n")
+        return compressed_dictionaries
+    
+    @staticmethod
+    def from_dictionaries_to_string(dictionaries):
+        result = ''
+        for index in range(len(dictionaries)):
+            if index != 0:
+                result += " and "
+            result += dictionaries[index]['column'] + " " + dictionaries[index]['symbol'] + " " + str(dictionaries[index]['value'])
+        
+        return result
+    
     def preprocessing(self, x, y):
         y = self.transform_y_to_all_results(x, y)  # transforms y so that it will have all the attributes as X
         y = OneHotEncoding().encoder(y, x)
@@ -263,137 +294,11 @@ class Utility:
         imp_index = 1
         result = pd.DataFrame()
         new_list_y = [0] * len(list_y)
-        print('list_y:')
-        print(list_y)
         while 1 in y.isfree.tolist() or 0 in y.isfree.tolist():
             most_important = collections.Counter(important_nodes).most_common(imp_index)[imp_index - 1][0]
-            print('most important:')
-            print(most_important)
             imp_index += 1
             # Search for all the elements that are in the node 'c'
             for elem in range(y.shape[0]):
-                print('elem:')
-                print(elem)
-                list_y_index = 0
-                y_ref = 0
-                if important_nodes[elem] == most_important:
-                    if y.isfree.iloc[elem] == 1:
-                        while y_ref != elem + 1:
-                            if list_y[list_y_index] == 1:
-                                y_ref += 1
-                            list_y_index += 1
-                        new_list_y[list_y_index] = 1
-                        result = pd.concat([result, y.iloc[[elem]]], axis=0)
-                        # for loop on the elements of the set to set the isfree column = -1
-                        for row in range(y.shape[0]):
-                            if y.tupleset.iloc[row] == y.tupleset.iloc[elem]:
-                                y.isfree.iloc[row] = -1
-
-                    elif y.isfree.iloc[elem] == 0:
-                        while y_ref != elem + 1:
-                            if list_y[list_y_index] == 1:
-                                y_ref += 1
-                            list_y_index += 1
-
-                        new_list_y[list_y_index] = 1
-                        result = pd.concat([result, y.iloc[[elem]]], axis=0)
-
-                    y.isfree.iloc[elem] = -1
-
-        result = result.drop(axis=1, columns=["isfree"])
-        return classifier, result, new_list_y
-
-
-
-    def min_altitude_first(self, dataframe_x, y, list_y, classifier):
-
-        # The decision estimator has an attribute called tree_  which stores the entire
-        # tree structure and allows access to low level attributes. The binary tree
-        # tree_ is represented as a number of parallel arrays. The i-th element of each
-        # array holds information about the node `i`. Node 0 is the tree's root. NOTE:
-        # Some of the arrays only apply to either leaves or split nodes, resp. In this
-        # case the values of nodes of the other type are arbitrary!
-        #
-        # Among those arrays, we have:
-        #   - left_child, id of the left child of the node
-        #   - right_child, id of the right child of the node
-        #   - feature, feature used for splitting the node
-        #   - threshold, threshold value at the node
-
-        print('SHAPE[0] DI X:')
-        print(dataframe_x.shape[0])
-        print('len list_y :')
-        print(len(list_y))
-                                                  
-        n_nodes = classifier.tree_.node_count
-        children_left = classifier.tree_.children_left
-        children_right = classifier.tree_.children_right
-        feature = classifier.tree_.feature
-        threshold = classifier.tree_.threshold
-
-        # The tree structure can be traversed to compute various properties such
-        # as the depth of each node and whether or not it is a leaf.
-        node_depth = np.zeros(shape=n_nodes, dtype=np.int64)
-        is_leaves = np.zeros(shape=n_nodes, dtype=bool)
-        stack = [(0, -1)]  # seed is the root node id and its parent depth
-        while len(stack) > 0:
-            node_id, parent_depth = stack.pop()
-            node_depth[node_id] = parent_depth + 1
-
-            # If we have a test node
-            if (children_left[node_id] != children_right[node_id]):
-                stack.append((children_left[node_id], parent_depth + 1))
-                stack.append((children_right[node_id], parent_depth + 1))
-            else:
-                is_leaves[node_id] = True
-
-        print("The binary tree structure has %s nodes and has "
-              "the following tree structure:"
-              % n_nodes)
-        for i in range(n_nodes):
-            if is_leaves[i]:
-                print("%snode=%s leaf node." % (node_depth[i] * "\t", i))
-            else:
-                print("%snode=%s test node: go to node %s if X[:, %s] <= %s else to "
-                      "node %s."
-                      % (node_depth[i] * "\t",
-                         i,
-                         children_left[i],
-                         feature[i],
-                         threshold[i],
-                         children_right[i],
-                         ))
-        print(node_depth)
-        important_nodes = self.most_important_nodes_generator(list_y, classifier, dataframe_x)
-        altitude_of_important_nodes = [-1] * len(important_nodes)
-        print('important nodes:')
-        print(important_nodes)
-        print('altitude_of_important_nodes')
-        print(altitude_of_important_nodes)
-        index = 0
-        for important_node in important_nodes:
-            altitude_of_important_nodes[index] = node_depth[important_node]
-            index += 1
-        print('altitude_of_important_nodes')
-        print(altitude_of_important_nodes)
-        print(min(altitude_of_important_nodes))
-        result = pd.DataFrame()
-        new_list_y = [0] * len(list_y)
-        print('list y = ')
-        print(list_y)
-        while 1 in y.isfree.tolist() or 0 in y.isfree.tolist():
-            #most_important = collections.Counter(important_nodes).most_common(imp_index)[imp_index - 1][0]
-            min_altitude = min(altitude_of_important_nodes)
-            min_altitude_index = None
-            i = 0
-            while min_altitude_index == None:
-                if altitude_of_important_nodes[i] == min_altitude:
-                    min_altitude_index = i
-                i += 1
-            most_important = important_nodes[min_altitude_index]
-            altitude_of_important_nodes[min_altitude_index] = 100
-            # Search for all the elements that are in the node 'c'
-            for elem in range(y.shape[0]):
                 list_y_index = 0
                 y_ref = 0
                 if important_nodes[elem] == most_important:
@@ -402,7 +307,9 @@ class Utility:
                             if list_y[list_y_index] == 1:
                                 y_ref += 1
                                 new_list_y[list_y_index] = 1
+
                             list_y_index += 1
+
                         result = pd.concat([result, y.iloc[[elem]]], axis=0)
                         # for loop on the elements of the set to set the isfree column = -1
                         for row in range(y.shape[0]):
@@ -415,6 +322,7 @@ class Utility:
                                 y_ref += 1
                                 new_list_y[list_y_index] = 1
                             list_y_index += 1
+
                         result = pd.concat([result, y.iloc[[elem]]], axis=0)
 
                     y.isfree.iloc[elem] = -1
